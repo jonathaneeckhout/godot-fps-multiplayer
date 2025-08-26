2640bf26c091bf65629f2d98822220975e0d9650
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2025-08-26 20:36:42 +0200

    move moving target

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -4,4 +4,5 @@ extends Node3D
 func _ready() -> void:
     if multiplayer.is_server():
         var moving_target: MovingTarget = load("res://scenes/moving_target/moving_target.tscn").instantiate()
+        moving_target.move = true
         %NetworkNodes.add_child(moving_target)
--- scenes/moving_target/moving_target.gd
+++ scenes/moving_target/moving_target.gd
@@ -1,2 +1,41 @@
 class_name MovingTarget
 extends StaticBody3D
+
+
+@export var move: bool = false
+@export var move_distance: float = 10.0
+@export var move_speed: float = 5.0
+
+var network_node: NetworkNode = null
+
+var start_position: Vector3
+var direction: int = 1
+var left_target: Vector3
+var right_target: Vector3
+
+func _ready() -> void:
+    start_position = position
+
+    left_target = start_position - Vector3.RIGHT * move_distance
+    right_target = start_position + Vector3.RIGHT * move_distance
+
+    network_node = get_node_or_null("NetworkNode")
+    assert(network_node != null, "Missing NetworkNode")
+
+    # This node should only run on the server
+    if network_node.mode != NetworkNode.Modes.SERVER:
+        set_physics_process(false)
+        return
+
+
+func _physics_process(delta: float) -> void:
+    if not move:
+        return
+
+    var target_position = right_target if direction == 1 else left_target
+
+    var new_position = position.move_toward(target_position, move_speed * delta)
+    position = new_position
+
+    if position.distance_to(target_position) < 0.1:
+        direction *= -1
