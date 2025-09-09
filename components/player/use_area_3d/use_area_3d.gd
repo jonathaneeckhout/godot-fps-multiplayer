e79c0dba1122a0818f3775ef95af1807312c29e5
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2025-09-09 23:19:34 +0200

    continue

--- components/player/use_area_3d/use_area_3d.gd
+++ components/player/use_area_3d/use_area_3d.gd
@@ -6,14 +6,35 @@ signal gun_left_range(weapon: WeaponLocation)
 
 var detected_gun: WeaponLocation = null
 
+var player: Player = null
+var network_node: NetworkNode = null
+var player_input: PlayerInput = null
+
+func _ready() -> void:
+    player = get_parent()
+    assert(player != null)
+
+    network_node = player.get_node_or_null("NetworkNode")
+    assert(network_node != null, "Missing NetworkNode")
+
+    player_input = player.get_node_or_null("PlayerInput")
+    assert(player_input != null, "PlayerInput not found")
+
+    if network_node.mode != NetworkNode.Modes.SERVER:
+        set_physics_process(false)
+
+
+func _physics_process(_delta: float) -> void:
+    pass
+
 
 func _on_area_shape_entered(_area_rid: RID, area: Area3D, _area_shape_index: int, _local_shape_index: int) -> void:
     if not area is WeaponLocation:
         return
-    
+
     if detected_gun != null:
         return
-    
+
     detected_gun = area
 
     gun_entered_range.emit(detected_gun)
